6a23d674a430aff47fe42410158301cbbe2662e5
Carol <carolinecollaco@gmail.com>
2014-08-06 00:39:47 -0300

    Initial commit

---
+++ js/Board.js
@@ -0,0 +1,204 @@
+var BoardClass = function(){
+	this.piecePosition = {col: 0, row: 0};
+	
+	this.boardState = [
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+	];
+	
+	this.cols = this.boardState[0].length;
+	this.rows = this.boardState.length;
+	this.piece = [];
+};
+
+BoardClass.prototype = function(){ 
+
+	var pieces = [
+		[
+			['#',' '],
+			['#',' '],
+			['#','#']
+		],
+		[
+			[' ','#'],
+			[' ','#'],
+			['#','#']
+		],
+		[
+			['#','#',' '],
+			[' ','#','#']
+		],
+		[
+			[' ','#','#'],
+			['#','#',' ']
+		],
+		[
+			['#','#','#'],
+			[' ','#',' ']
+		],
+		[
+			['#','#'],
+			['#','#']
+		],
+		[
+			['#'],
+			['#'],
+			['#'],
+			['#']
+		]
+	];
+	
+	var start = function(){
+		this.piece = getRandomPiece();	
+	}
+
+	function getRandomArbitrary(min, max) {
+	  return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	function getRandomPiece(){
+		return pieces[ getRandomArbitrary(0, pieces.length)];
+	}
+
+	var fixPieceOnBoard = function(){
+		var piecePosition = this.piecePosition;
+		for(var pieceRow = 0; pieceRow < this.piece.length; pieceRow++){
+			for(var pieceCol = 0; pieceCol < this.piece[pieceRow].length; pieceCol++){
+				if(this.piece[pieceRow][pieceCol] == '#')
+					this.boardState[piecePosition.row+pieceRow][piecePosition.col+pieceCol] = '#';
+			}
+		}
+	}
+
+	var rotateThisPiece = function(pieceToRotate){
+		var newPiece = new Array(pieceToRotate[0].length);
+		for(var npieceRow = 0; npieceRow < newPiece.length; npieceRow++){
+			newPiece[npieceRow] = new Array(pieceToRotate.length);
+		}
+
+		for(var pieceRow = 0; pieceRow < pieceToRotate.length; pieceRow++){
+			for(var pieceCol = pieceToRotate[pieceRow].length -1; pieceCol >= 0; pieceCol--){
+				var pos = pieceToRotate[pieceRow].length -1 - pieceCol;
+				newPiece[pos][pieceRow] = pieceToRotate[pieceRow][pieceCol];
+			}
+		}
+		return newPiece;
+	}
+
+	var rotatePiece = function(){
+		var piecePosition = this.piecePosition;
+		this.piece = rotateThisPiece.call(this,this.piece);
+		while(piecePosition.col+this.piece[0].length > this.cols){
+			piecePosition.col--;
+		}
+	}
+
+	var throwANewPiece = function(){
+		this.piece = pieces[ getRandomArbitrary(0, pieces.length)];
+		this.piecePosition.row = 0;
+		this.piecePosition.col = getRandomArbitrary(0,this.cols-this.piece[0].length +1);
+	}
+
+	var thereIsSomethingBelowThePiece = function(){
+		var piecePosition = this.piecePosition;
+		for(var pieceRow = 0; pieceRow < this.piece.length; pieceRow++){
+			for(var pieceCol = 0; pieceCol < this.piece[pieceRow].length; pieceCol++){
+				if(this.piece[pieceRow][pieceCol] == '#' && this.boardState[piecePosition.row+pieceRow+1][piecePosition.col+pieceCol] == '#'){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	function checkIfLineIsFull(line){
+		for(var col = 0; col < this.cols; col++){
+			if(this.boardState[line][col] == ' ')
+				return false;
+		}
+		return true;
+	}
+
+	var dropLinesAbove = function(line){
+		for(var row = line; row > 0; row--){
+			for(var col = 0; col < this.cols; col++){
+				this.boardState[row][col] = this.boardState[row-1][col];
+			}
+		}
+	}
+
+	var removeLines = function(){
+		var linesToRemove = [];
+		for(var row = this.rows -1 ; row >= 0; row--){
+			if(checkIfLineIsFull.call(this,row)){
+				linesToRemove.push(row);
+			}
+		}
+
+		var removedCount = 0;
+		for(var line = 0; line < linesToRemove.length; line++){
+			dropLinesAbove.call(this,linesToRemove[line]+removedCount);
+			removedCount++;
+		}
+	}
+
+	var step = function(){
+		if(this.piecePosition.row+this.piece.length == this.rows || thereIsSomethingBelowThePiece.call(this)){
+			fixPieceOnBoard.call(this);
+			throwANewPiece.call(this);
+			removeLines.call(this);
+		}else{
+			this.piecePosition.row+=1;
+		}
+	}
+	
+	var getPieceWithPosition = function(){
+		return {
+			piece : this.piece,
+			col : this.piecePosition.col,
+			row : this.piecePosition.row
+		};
+	}
+	
+	function moveRight(){
+		if(this.piecePosition.col + this.piece[0].length < this.cols)
+			this.piecePosition.col++;
+	}
+	
+	function moveLeft(){
+		if(this.piecePosition.col > 0)
+			this.piecePosition.col--;
+	}
+	
+	return {
+		start : start,
+		step : step,
+		getPieceWithPosition : getPieceWithPosition,
+		moveRight : moveRight,
+		moveLeft : moveLeft,
+		rotatePiece : rotatePiece
+	};
+}();
+
+var Board = new BoardClass();
+var Board2 = new BoardClass();
---
+++ js/Main.js
@@ -0,0 +1,153 @@
+var canvas= document.getElementById("game-canvas");
+var context= canvas.getContext("2d");
+context.font = "bold 20px Arial";
+context.strokeStyle = "black";
+context.lineWidth = 3;
+context.fillStyle = "white";
+
+function fillStrokedText(text, x, y){
+    console.log("'"+text+"' on "+x+","+y);
+	context.strokeText(text, x, y);
+	context.fillText(text, x, y);
+}
+
+var pieceSize = Math.floor(canvas.height/22);
+
+function textHeight(text){
+    var d = document.createElement("span");
+    d.font = context.font;
+    d.textContent = text;
+    document.body.appendChild(d);
+    var emHeight = d.offsetHeight;
+    document.body.removeChild(d);
+    return emHeight;
+}
+
+var textHeight = textHeight("ColorTetris");
+//fillStrokedText("ColorTetris", 0, textHeight);
+
+
+context.lineWidth = 1;
+var boardX = 0;//3;
+var boardY = 0;//textHeight+10;
+
+function draw(){
+    context.fillStyle = "white";
+	
+	var pieceWithPosition = Board.getPieceWithPosition();
+	
+	var pieceColPos = pieceWithPosition.col;
+	var pieceRowPos = pieceWithPosition.row;
+	var piece = pieceWithPosition.piece;
+	var boardState = Board.boardState;
+	var rows = boardState.length;
+	var cols = boardState[0].length;
+	
+    for(var row = 0; row < rows; row++){
+        for(var col = 0; col < cols; col++){
+            context.fillRect(col*pieceSize + boardX, row*pieceSize + boardY , pieceSize, pieceSize);
+            context.strokeRect(col*pieceSize + boardX, row*pieceSize + boardY , pieceSize, pieceSize);
+        }
+    }
+    
+    context.fillStyle = "green";
+    for(var row = 0; row < rows; row++){
+        for(var col = 0; col < cols; col++){
+            if(boardState[row][col] == '#')
+                context.fillRect(col*pieceSize + boardX, row*pieceSize + boardY , pieceSize, pieceSize);
+        }
+    }
+    
+    for(var pieceRow = 0; pieceRow < piece.length; pieceRow++){
+        for(var pieceCol = 0; pieceCol < piece[pieceRow].length; pieceCol++){
+            if(piece[pieceRow][pieceCol] == '#')
+                context.fillRect((pieceColPos+pieceCol)*pieceSize + boardX, (pieceRowPos+pieceRow)*pieceSize + boardY , pieceSize, pieceSize);
+        }
+    }
+	
+	for(var row = 0; row < rows; row++){
+        for(var col = 0; col < cols; col++){
+            context.strokeRect(col*pieceSize + boardX, row*pieceSize + boardY , pieceSize, pieceSize);
+        }
+    }
+}   
+
+function pieceScores(){
+	for(var pieceIndex = 0; pieceIndex < pieces.length;  pieceIndex++){
+		var piece = pieces[pieceIndex];
+		var score = evaluatePiece(piece);
+	}
+}
+
+var oneSecond = 1000;
+var FPS = 30;
+var frameLimit = Math.floor(oneSecond/FPS);
+var timePassed = 0;
+var lastLoopTime = Date.now();
+var delta = 0;
+
+var speed = 1;
+
+var lastMoveTime = 0;
+
+Board.start();
+
+function loop(){
+    var currentTime = Date.now();
+	delta += currentTime - lastLoopTime;
+	if(delta < frameLimit){
+		lastLoopTime = Date.now();
+		requestAnimationFrame(loop);
+		return;	
+	}
+	timePassed+=frameLimit;
+	var timePassedInSeconds = Math.floor(timePassed/1000);
+    
+    if(timePassedInSeconds-lastMoveTime >= speed){
+		lastMoveTime = timePassedInSeconds;
+        
+        Board.step();
+		
+	}
+    
+    draw();
+    
+    lastLoopTime = Date.now();
+	delta = 0;
+	requestAnimationFrame(loop);			
+}
+
+requestAnimationFrame(loop);
+
+var left = 37;
+var up = 38;
+var right = 39;
+var down = 40;
+
+document.onkeydown = function(e) {
+	if(e.keyCode === right){
+		Board.moveRight();
+		
+		e.preventDefault();
+	}
+	if(e.keyCode === left){
+		Board.moveLeft();
+		
+		e.preventDefault();
+	}
+	if(e.keyCode === down){
+		speed = 0;
+		e.preventDefault();
+	}
+	if(e.keyCode === up){
+		Board.rotatePiece();
+		e.preventDefault();
+	}
+};
+
+document.onkeyup = function(e) {
+	if(e.keyCode === down){
+		speed = 1;
+		e.preventDefault();
+	}
+};
